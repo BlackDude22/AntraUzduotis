f93a181445d858ae939ce229737e52db5bbfead0
Lukas <lukislt7@gmail.com>
2018-02-15 00:03:23 +0200

    Atnaujintas kodas
    Pazymius skaiciuoji iki ivedamas 0

--- antra_uzduotis.cpp
+++ antra_uzduotis.cpp
@@ -14,11 +14,11 @@ int main()
     std::cout << "Studento pavarde: ";
     std::string pavarde;
     std::cin >> pavarde;
-
+/*
     std::cout << "Namu darbu skaicius: ";
     int n = 0;
     std::cin >> n;
-
+*/
     char generuoti;
     while (true){
         std::cout << "Ar norite generuoti pazymius atsitiktinai?(t/n): ";
@@ -31,7 +31,8 @@ int main()
     std::cout << std::endl;
     std::cout << "Namu darbu pazymiai: " << std::endl;
 
-    std::vector<int> nDarbas(n);
+    std::vector<int> nDarbas;
+    /*
     int i = 0;
     if (generuoti == 'n' || generuoti == 'N'){
         while (i < n){
@@ -50,6 +51,46 @@ int main()
             nDarbas[i] = mark(mt);
             std::cout << i+1 << ". " << nDarbas[i] << std::endl;
             i++;
+        }
+    }
+*/
+
+    int i = 0;
+    if (generuoti == 'n' || generuoti == 'N'){
+        while (true){
+            int temp;
+            std::cout << i+1 << ". ";
+            std::cin >> temp;
+            nDarbas.push_back(temp);
+            if (nDarbas[i] == 0 && i == 0){
+                nDarbas.pop_back();
+                std::cout << "Iveskite bent viena pazymi!" << std::endl;
+            }
+            else if (nDarbas[i] == 0){
+                nDarbas.pop_back();
+                break;
+                }
+            else if (nDarbas[i] > 10 || nDarbas[i] < 1){
+                std::cout << "Tokio pazymio negali buti!" << std::endl;
+            }
+            else i++;
+        }
+    }
+    else if (generuoti == 't' || generuoti == 'T'){
+        std::mt19937 mt(time(0));
+        std::uniform_int_distribution<int> mark(0,10);
+        while (true){
+            nDarbas.push_back(mark(mt));
+            std::cout << i+1 << ". " << nDarbas[i] << std::endl;
+            if (nDarbas[i] == 0 && i == 0){
+                nDarbas.pop_back();
+                std::cout << "Iveskite bent viena pazymi!" << std::endl;
+            }
+            else if (nDarbas[i] == 0){
+                nDarbas.pop_back();
+                break;
+                }
+            else i++;
         }
     }
 
@@ -76,17 +117,17 @@ int main()
     if(mediana == 'n' || mediana == 'N'){
         std::cout << "Galutinis balas: ";
         int galBal = 0;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < nDarbas.size(); i++)
             galBal += nDarbas[i];
-        std::cout << std::fixed << std::setprecision(2) << 0.4*galBal/n+0.6*egzaminas << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << 0.4*galBal/nDarbas.size()+0.6*egzaminas << std::endl;
     }
     else if (mediana == 't' || mediana == 'T'){
         std::cout << "Galutinis balas: ";
         std::sort(nDarbas.begin(), nDarbas.end());
         double galBal = 0;
-        if (n%2 == 1)
-            galBal = nDarbas[n/2];
-        else galBal = (double)(nDarbas[n/2]+nDarbas[n/2+1])/2;
+        if (nDarbas.size()%2 == 1)
+            galBal = nDarbas[nDarbas.size()/2];
+        else galBal = (double)(nDarbas[nDarbas.size()/2]+nDarbas[nDarbas.size()/2+1])/2;
         std::cout << std::fixed << std::setprecision(2) << 0.4*galBal+0.6*egzaminas << std::endl;
     }
 }
